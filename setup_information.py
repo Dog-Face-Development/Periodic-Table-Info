344ac968731653a71403e0241840ee4a7c1143cd
William <willtheorangeguy@outlook.com>
2016-07-04 15:53:45 -0700

    Python 3.5.2 Update and Continuing Develepment
    The updated Python files for the Python update 3.5.2 and continuing development file updates.

---
+++ setup_information.py
@@ -0,0 +1,16 @@
+# Setup Inofrmation
+#This is not an important Python script!
+# Just a script with my information.
+
+import os
+import sys
+
+setup(
+    name='Periodic Table Info'
+    version='1.0',
+    author = 'William Vandergraaf',
+    author_email = 'willtheorangeguy@outlook.com',
+    url = 'https://github.com/dog-face-development/Periodic-Table-Info',
+    license='MIT',
+    description= "This program is a great way to learn more about the diffrent elements of the Periodic Table",
+)
